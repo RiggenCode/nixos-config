8e64968ea5627554c63bf1bd402ab0678f60274d
Riggen Snyder <riggen.snyder@gmail.com>
2026-02-03 16:05:05 -0600

    added hyprland stuff

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 { config, pkgs, ... }:
 
@@ -51,8 +51,15 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -83,7 +90,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.riggens = {
     isNormalUser = true;
     description = "Riggen Snyder";
@@ -106,7 +113,23 @@
     neofetch
     prismlauncher
     git
-    gh 
+    gh
+    
+    # Hyprland essentials
+    kitty              # Terminal (or use alacritty, foot, etc.)
+    wofi               # Application launcher (rofi for wayland)
+    waybar             # Status bar
+    dunst              # Notification daemon
+    swww               # Wallpaper daemon
+    grim               # Screenshot tool
+    slurp              # Screen area selection for screenshots
+    wl-clipboard       # Clipboard utilities
+    
+    # Useful utilities for ricing
+    brightnessctl      # Brightness control
+    playerctl          # Media player control
+    pamixer            # Audio control
+    networkmanagerapplet # Network manager applet
   ];
 
 
@@ -131,7 +154,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
